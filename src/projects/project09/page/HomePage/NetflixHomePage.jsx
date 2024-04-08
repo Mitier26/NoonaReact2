8c2d5de10d2a750e744e436d57ce71eebf0f1500
Molkang <mitier26@gmail.com>
2024-04-08 19:13:06 +0900

    넥플릭스

--- src/projects/project09/page/HomePage/NetflixHomePage.jsx
+++ src/projects/project09/page/HomePage/NetflixHomePage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Banner from './components/Banner/Banner';
-
+import PopularMoviesSlide from './components/PopularMoviesSlide/PopularMoviesSlide';
+import TopRatedMoviesSlide from './components/TopRatedMoviesSlide/TopRatedMoviesSlide';
+import UpcomingMoviesSlide from './components/UpcomingMoviesSlide/UpcomingMoviesSlide';
 // 베너 : 인기영화를 가지고과 보여준다.
 
 const NetflixHomePage = () => {
     return (
         <div>
             <Banner></Banner>
+            <PopularMoviesSlide />
+            <TopRatedMoviesSlide />
+            <UpcomingMoviesSlide />
         </div>
     );
 };
---
+++ src/projects/project09/page/HomePage/components/TopRatedMoviesSlide/TopRatedMoviesSlide.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useTopRatedMovies } from '../../../../hooks/useTopRatedMovies';
+import { Alert } from 'bootstrap';
+import MovieSlider from '../../../../common/MovieSlider/MovieSlider';
+import { responsive } from '../../../../constants/responsive';
+
+const TopRatedMoviesSlide = () => {
+    const { data, isLoading, isError, error } = useTopRatedMovies();
+
+    if (isLoading) {
+        return <h1>Loading...</h1>;
+    }
+
+    if (isError) {
+        return <Alert variant="danger">{error.message}</Alert>;
+    }
+
+    return (
+        <div>
+            <MovieSlider title="Top Rated Movies" movies={data.results} responsive={responsive} />
+        </div>
+    );
+};
+
+export default TopRatedMoviesSlide;
